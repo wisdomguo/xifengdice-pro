40dad77d93a7109d1309756ed1820b38c247dd7b
19690 <1969077760@qq.com>
2021-04-09 14:01:07 +0800

    改变包名

---
+++ src/main/java/com/wisdomguo/xifeng/service/skillcoc/SkillCocService.java
@@ -0,0 +1,14 @@
+package com.wisdomguo.xifeng.service.skillcoc;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.wisdomguo.xifeng.entity.SkillCoC;
+
+/**
+ * SkillCocService
+ *
+ * @author wisdom-guo
+ * @since 2021/4/3
+ */
+public interface SkillCocService extends IService<SkillCoC> {
+
+}
