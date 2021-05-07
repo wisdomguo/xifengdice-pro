c0e3922cdc546beaf7a3eaab3e1d8964adcc0ff4
19690 <1969077760@qq.com>
2021-05-07 11:11:41 +0800

    【专用】新增取消排班

--- src/main/java/com/wisdomguo/xifeng/plugin/HarukaBotPlugin.java
+++ src/main/java/com/wisdomguo/xifeng/plugin/HarukaBotPlugin.java
@@ -67,7 +67,7 @@ public class HarukaBotPlugin extends BotPlugin {
     @SneakyThrows
     @Override
     public int onGroupMessage(@NotNull Bot cq, @NotNull OnebotEvent.GroupMessageEvent event) {
-        // 获取 消息内容 群号 发送者QQ
+
         //获取消息内容
         String msg = event.getRawMessage().replaceAll("。",".");
         //获取群号
